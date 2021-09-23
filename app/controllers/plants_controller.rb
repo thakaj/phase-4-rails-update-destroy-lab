185e3b6e3d0828da8c2a5b730975306889f3c87f
thakaj <taulant.hakaj@scranton.edu>
2021-09-23 16:35:59 -0400

    commit tree 4f02a8039f03c37618bff0728d74ab9ae5f8f008

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
   # GET /plants
   def index
@@ -17,9 +18,32 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+    plant.update(plant_params)
+    render json: plant
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+    plant.destroy
+    head :no_content
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
 
   private
+  def render_not_found
+    render json: {error: "Bird not found"}, status: :not_found
+  end
 
+ 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
